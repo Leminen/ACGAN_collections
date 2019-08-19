d6bd973901b3683be8c44642b16026399c00ea60
leminen <slm@eng.au.dk>
2019-08-19 10:44:29 +0200

    quick fix

--- src/models/WacGAN_info.py
+++ src/models/WacGAN_info.py
@@ -173,7 +173,7 @@ class WacGAN_info(object):
 
         elif dataset == 'PSD_Segmented':
             # self.dataset_filenames = ['data/processed/PSD_Segmented_Anders/data_shard_{:03d}-of-{:03d}.tfrecord'.format(i+1,psd_dataset._NUM_SHARDS) for i in range(psd_dataset._NUM_SHARDS)]
-            self.dataset_filenames = ['data/processed/PSD_Segmented/data_shard_{:03d}-of-{:03d}.tfrecord'.format(i,psd_dataset._NUM_SHARDS) for i in range(psd_dataset._NUM_SHARDS)]
+            self.dataset_filenames = ['data/processed/PSD_Segmented/data_shard_{:03d}-of-{:03d}.tfrecord'.format(i+1,psd_dataset._NUM_SHARDS) for i in range(psd_dataset._NUM_SHARDS)]
             self.lbls_dim = 9
             self.image_dims = [128,128,3]
 
